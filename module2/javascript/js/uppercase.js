f336c9d06703c00c2220149227ed01e2460a197a
Brijesh1990 <bkpandey.pandey@gmail.com>
2022-10-11 15:04:33 +0530

    Add files via upload

---
+++ module2/javascript/js/uppercase.js
@@ -0,0 +1,11 @@
+function chng()
+{
+    var fnm=document.getElementById("txt_fnm");
+    fnm.value=fnm.value.toUpperCase();
+
+    var lnm=document.getElementById("txt_lnm");
+    lnm.value=lnm.value.toUpperCase();
+
+    var em=document.getElementById("txt_email");
+    em.value=em.value.toUpperCase();
+}
